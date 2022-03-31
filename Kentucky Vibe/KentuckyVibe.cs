3e9385fe5309fa12cfb9822f462cb3b89ab398c2
Jeff Martin <Jeffm3777@gmail.com>
2022-03-30 21:38:31 -0400

    Got Program running again

--- Kentucky Vibe/KentuckyVibe.cs
+++ Kentucky Vibe/KentuckyVibe.cs
@@ -10,40 +10,20 @@ namespace KentuckyVibe
     {
         public string CustomerID { get; set;}
         public string EmailAddress { get; set;}
-        public string First name { get; set;}
-        public String Last name { get; set;}
-        public bool Validate() { }
-        public bool Retrieve() { }
-        public bool Save() { }
+        public string FirstName { get; set;}
+        public String LastName { get; set;}
+        public bool Validate() { return true; }
+        public bool Retrieve() { return true; }
+        public bool Save() { return true; }
     }
     public class Product
 {
-    public string Product { get; set; }
-    public string Product Name { get; set; }
-    public string Poduct Price { get; set; }
-    public bool Validate() { }
-    public bool Retrieve() { }
-    public bool Save() { }
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public string Price { get; set; }
+    public bool Validate() { return true; }
+    public bool Retrieve() { return true; }
+    public bool Save() { return true; }
 }
-    public class Order 
-{   
-    public string Customer { get; set; }
-    public string Product name { get; set; }
-    public string Product Price { get; set; }
-    public bool Validate() { }
-    public bool Retrieve() { }
-    public bool Save() { }
-
-}
-
-    public class Order Iteam
-{
-    public string Order Iteam { get; set; }
-    public string Order Date { get; set; }
-    public string Order Time{ get; set; }
-    public String Customer name { get; set; }
-    public bool Validate() { }
-    public bool Retrieve () { }
-    public bool Save () { }
-}
+    
 }
--- Kentucky Vibe/Program.cs
+++ Kentucky Vibe/Program.cs
@@ -1,4 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
  using System;
-Console.WriteLine("Hello, Welcome to Kentucky Vibe! ");
-Console.WriteLine("Please put in you name");
+
+
+namespace KentuckyVibe
+{
+
+    class Program
+    {
+        static void Main ()
+        {
+            Console.WriteLine("Hello,Welcome to Kentucky Vibe");
+           
+            List<Customer> customers = new List<Customer> ();
+            var customer = new Customer ();
+            Console.WriteLine("Please put in your firstname");
+            customer.FirstName = Console.ReadLine ();
+
+            Console.WriteLine("Please put in your lastname");
+            customer.LastName = Console.ReadLine ();
+            customers.Add (customer);
+            Console.WriteLine("Here is the list of customers");
+            customers.ForEach (customer => { Console.WriteLine(customer.FirstName+" "+customer.LastName); });
+        }
+    }
+}
