8adc0a9523091ddd404ef57487a36d2b83586e37
Jeff Martin <Jeffm3777@gmail.com>
2022-04-02 17:32:56 -0400

    Added a master loop

--- Kentucky Vibe/KentuckyVibe.cs
+++ Kentucky Vibe/KentuckyVibe.cs
@@ -19,6 +19,8 @@ namespace KentuckyVibe
         public bool Validate() { return true; }
         public bool Retrieve() { return true; }
         public bool Save() { return true; }
+
+        
     }
     public class Product
 {
--- Kentucky Vibe/Program.cs
+++ Kentucky Vibe/Program.cs
@@ -10,17 +10,28 @@ namespace KentuckyVibe
         static void Main ()
         {
             Console.WriteLine("Hello,Welcome to Kentucky Vibe");
-           
-            List<Customer> customers = new List<Customer> ();
-            var customer = new Customer ();
-            Console.WriteLine("Please put in your firstname");
-            customer.FirstName = Console.ReadLine ();
+            Console.WriteLine("DO you want to Register (Y/N)");
+            var registerResponse = Console.ReadLine().ToLower();
 
-            Console.WriteLine("Please put in your lastname");
-            customer.LastName = Console.ReadLine ();
-            customers.Add (customer);
-            Console.WriteLine("Here is the list of customers");
-            customers.ForEach (customer => { Console.WriteLine(customer.FullName); });
+            while (registerResponse == "y")
+
+            {
+                //This code runs until the while condition is false.
+                List<Customer> customers = new List<Customer>();
+                var customer = new Customer();
+                Console.WriteLine("Please put in your firstname");
+                customer.FirstName = Console.ReadLine();
+
+                Console.WriteLine("Please put in your lastname");
+                customer.LastName = Console.ReadLine();
+                customers.Add(customer);
+                Console.WriteLine("Here is the list of customers");
+                customers.ForEach(customer => { Console.WriteLine(customer.FullName); });
+                Console.WriteLine("DO you want to Register (Y/N)");
+                registerResponse = Console.ReadLine().ToLower();
+
+            }
+
         }
     }
 }
